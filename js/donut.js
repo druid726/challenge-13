8f6bd1bcadc1dd0b89d8f833ac817399a125659f
druid726 <pdoneill26@gmail.com>
2014-04-07 20:56:31 -0400

    added js

--- js/donut.js
+++ js/donut.js
@@ -1,5 +1,34 @@
 $(function(){
 
   //write code here...
+  var  Donut = {
+  	   type: 'glazed',
+  	   color: 'white',
+  	   tasty: 'yes',
+  	el: function() {
+  		return '<li>'+
+  			this.type+' <br>'+
+  			this.color+' <br>'+
+  			this.tasty+
+  			'</li>';
+  	},
+  	create: function() {
+  		$('#donut-list').append(this.el());
+  	}
+  };
+Donut.all = [];
+
+$('#create').click(function(){
+    var type = $('#type').val();
+    var color = $('#color').val();
+    var tasty = $('#tasty').val();
+    var newDonut = Object.create(Donut);
+    newDonut.type = type;
+    newDonut.color = color;
+    newDonut.tasty = tasty;
+    Donut.all.push(newDonut);
+    //console.log(Donut.all);
+    newDonut.create();
+  });
 
 });
